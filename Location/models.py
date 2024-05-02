dea8ddda7933287b9089a422e48db5249f956f24
Raihan Sajid <raihansajid1911@gmail.com>
2024-05-02 12:55:56 +0600

    final update;)

--- Location/models.py
+++ Location/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class Location(models.Model):
-    Co_ordinate = models.CharField(max_length=200)
+    Area_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.Co_ordinate
+        return self.Area_name
 class Hospital(models.Model):
     H_ID = models.CharField(max_length=200)
     H_name = models.CharField(max_length=200)
-    Co_ordinate = models.ForeignKey(Location, on_delete=models.CASCADE)
+    Area_name = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     image = models.ImageField(upload_to='images/',blank=True, null=True, default='images/default.jpg')
 
@@ -19,7 +18,7 @@ class Hospital(models.Model):
         return self.H_ID
 
 class Ambulance(models.Model):
-    Co_ordinate = models.ForeignKey(Location, on_delete=models.CASCADE)
+    Area_name = models.ForeignKey(Location, on_delete=models.CASCADE)
     A_Number = models.CharField(max_length=200)
     def __str__(self):
         return self.A_Number
